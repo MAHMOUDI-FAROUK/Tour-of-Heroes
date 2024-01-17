d93236cba549ea5f2f9e2e92b58d5ae432bb3865
MAHMOUDI-FAROUK <mahmoudifaroukch1993@gmail.com>
2024-01-17 10:29:53 +0100

    edit one hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
+import {
+  /* . . . */
+  NgFor,
+  /* . . . */
+} from '@angular/common';
 
 @Component({
+  standalone: true,
+  imports: [
+    NgFor,
+  ],
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
+  
 })
 export class HeroesComponent {
-
+  heroes = HEROES;
 }
